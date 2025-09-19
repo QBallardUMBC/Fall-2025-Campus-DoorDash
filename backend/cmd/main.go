ae8ecf3ae9695f2008907b28220d86b4b61d6b3e
Tobi Abidoye <tobiabidoye14@gmail.com>
2025-09-18 20:00:41 -0400

    added cors config

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"campusDoordash/internal/auth"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 	"log"
 	_ "net/http"
+	"time"
 )
 
 func main() {
@@ -16,8 +18,24 @@ func main() {
 	auth.InitDb()
 	auth.SetupAuthClient()
 	router := gin.Default()
+	enableCors(router)
 	router.GET("/auth", auth.AuthHandler)
 	router.POST("/auth/register", auth.RegisterHandler)
 	router.POST("/auth/login", auth.LoginHandler)
 	router.Run(":8080")
+}
+
+func enableCors(e *gin.Engine) {
+	e.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"https://foo.com"},
+		AllowMethods:     []string{"PUT", "PATCH"},
+		AllowHeaders:     []string{"Origin"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		AllowOriginFunc: func(origin string) bool {
+			return origin == "https://github.com"
+		},
+		MaxAge: 12 * time.Hour,
+	}))
+
 }
