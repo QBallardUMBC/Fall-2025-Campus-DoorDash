b111a269d6c97988e87d76c0db4a8fb2dbfc3ed5
Tobi Abidoye <tobiabidoye14@gmail.com>
2025-10-08 15:45:58 -0400

    added a bit more functionality for resaturant data

--- backend/internal/restaurants/handlers.go
+++ backend/internal/restaurants/handlers.go
@@ -11,6 +11,10 @@ type RestaurantHandlers struct{
 	service * RestaurantService
 }
 
+type BatchFoodItemsRequest struct{
+	FoodIDs []uuid.UUID `json:"food_ids" binding:"required"`
+}
+
 func NewRestaurantHandler(service * RestaurantService) *RestaurantHandlers{
 	return &RestaurantHandlers{service: service} 
 }
@@ -48,6 +52,36 @@ func (h * RestaurantHandlers) GetRestaurantByID(c * gin.Context){
 	c.JSON(http.StatusOK, restaurant)
 }
 
+func (h* RestaurantHandlers) GetBatchRestaurantsByID(c * gin.Context){
+	var req BatchFoodItemsRequest
+	 
+	if err := c.ShouldBindJSON(&req); err != nil{
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid request body",})	
+		return
+	}
+	
+	if len(req.FoodIDs) == 0{
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": "food_ids cannot be empty",
+		})
+		return
+	}
+
+	items, err := h.service.GetFoodItemBYIDs(c.Request.Context(), req.FoodIDs)
+
+	if err != nil{
+		log.Printf("failed to fetch food items %v", err)
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"error":"failed to fetch food items", 
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"items": items,
+		"count": len(items),
+	})
+}
 
 func (h* RestaurantHandlers) GetRestaurantMenuHandler(c * gin.Context){
 	restaurantID, err := uuid.Parse(c.Param("id"))
@@ -70,6 +104,7 @@ func (h* RestaurantHandlers) GetRestaurantMenuHandler(c * gin.Context){
 	}
 
 	c.JSON(http.StatusOK, gin.H{"menu": menu})
+
 	
 }
 
